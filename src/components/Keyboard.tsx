460527b8dded93f1bc0ed9149cdfa2ccb0b59603
wvbe <wybe@x-54.com>
2022-01-02 11:29:42 +0000

    Add a veritable keyboard!

---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,202 @@
+import { FunctionComponent } from 'react';
+
+const KeyboardLetter: FunctionComponent<{
+	letter: string;
+	enabled: boolean;
+	isLamp: boolean;
+	onPress?: (letter: string) => void;
+}> = ({ letter, enabled, isLamp, onPress }) => {
+	let mark = [' ', ' '];
+	if (enabled) {
+		mark = isLamp ? ['[', ']'] : ['{', '}'];
+	}
+	const onClick = onPress
+		? () => {
+				onPress(letter);
+		  }
+		: undefined;
+	const className = [enabled ? 'enabled' : undefined, onClick ? 'has-click' : undefined]
+		.filter(Boolean)
+		.join(' ');
+	return (
+		<pre className={className} onClick={onClick}>
+			{mark[0] + String(letter) + mark[1]}
+		</pre>
+	);
+};
+
+const DEFAULT_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+export const Keyboard: FunctionComponent<{
+	alphabet?: string;
+	character?: number;
+	isLamp: boolean;
+	onLetterPress?: (letter: string) => void;
+}> = ({ alphabet = DEFAULT_ALPHABET, character, isLamp, onLetterPress }) => {
+	return (
+		<div className="keyboard">
+			<div>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('Q'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('Q')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('W'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('W')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('E'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('E')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('R'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('R')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('T'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('T')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('Y'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('Y')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('U'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('U')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('I'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('I')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('O'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('O')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('P'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('P')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+			</div>
+			<div>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('A'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('A')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('S'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('S')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('D'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('D')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('F'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('F')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('G'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('G')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('H'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('H')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('J'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('J')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('K'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('K')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('L'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('L')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+			</div>
+			<div>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('Z'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('Z')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('X'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('X')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('C'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('C')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('V'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('V')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('B'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('B')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('N'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('N')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+				<KeyboardLetter
+					letter={alphabet.charAt(DEFAULT_ALPHABET.indexOf('M'))}
+					enabled={character === DEFAULT_ALPHABET.indexOf('M')}
+					isLamp={isLamp}
+					onPress={onLetterPress}
+				/>
+			</div>
+		</div>
+	);
+};
--- src/components/Machine.tsx
+++ src/components/Machine.tsx
@@ -1,82 +1,71 @@
-import { FunctionComponent, useEffect, useState } from 'react';
+import { FunctionComponent, useEffect } from 'react';
 import { Machine as MachineClass } from '../classes/Machine';
 import { useEvent } from '../hooks/useEvent';
+import { Keyboard } from './Keyboard';
 import { Rotor } from './Rotor';
 
 export const Machine: FunctionComponent<{ instance: MachineClass }> = ({ instance }) => {
-	const [input, updateInput] = useState('');
-	const [output, updateOutput] = useState('');
 	const [offsets] = useEvent(instance.$encode, []);
 	useEffect(() => {
 		function handleKey(event: KeyboardEvent) {
-			if (input.length !== output.length) {
-				// Guard against multiple simultaneous input
-				return;
-			}
 			const key = event.key.toLowerCase();
 			if (key.length !== 1 || !key.match(/[a-z]/g)) {
 				return;
 			}
-			updateInput(input + key);
-			updateOutput(output + instance.encodeLetter(key));
+			instance.encodeLetter(key);
 		}
 		window.document.body.addEventListener('keypress', handleKey);
 		return () => {
 			window.document.body.removeEventListener('keypress', handleKey);
 		};
-	}, [instance, input, updateInput, output, updateOutput]);
+	}, [instance]);
 
 	return (
-		<div
-			style={{
-				position: 'absolute',
-				top: '50%',
-				left: '50%',
-				transform: 'translate(-50%, -50%)'
-			}}
-		>
-			<div className="scrambler">
-				{/* {instance.reflector && <Reflector instance={instance.reflector} />} */}
-				{instance.rotors
-					.slice()
+		<div className="machine">
+			{/* {instance.reflector && <Reflector instance={instance.reflector} />} */}
+			{instance.rotors
+				.slice()
+				.reverse()
+				.map((rotor, inverseIndex) => {
+					const rotorCount = instance.rotors.length;
+					const index = rotorCount - inverseIndex - 1;
+					return (
+						<Rotor
+							key={inverseIndex}
+							instance={rotor}
+							signalsTowardsReflector={[offsets[index], offsets[index + 1]]}
+							signalsTowardsLamps={[
+								offsets[rotorCount + 1 + inverseIndex],
+								offsets[rotorCount + 2 + inverseIndex]
+							]}
+						/>
+					);
+				})}
+			<pre>
+				{'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+					.split('')
+					.map((l, i) => {
+						if (offsets[0] === i) {
+							return `[${l}]`;
+						}
+						if (offsets[offsets.length - 1] === i) {
+							return `{${l}}`;
+						}
+						return ` ${l} `;
+					})
 					.reverse()
-					.map((rotor, inverseIndex) => {
-						const rotorCount = instance.rotors.length;
-						const index = rotorCount - inverseIndex - 1;
-						return (
-							<Rotor
-								key={inverseIndex}
-								instance={rotor}
-								signalsTowardsReflector={[offsets[index], offsets[index + 1]]}
-								signalsTowardsLamps={[
-									offsets[rotorCount + 1 + inverseIndex],
-									offsets[rotorCount + 2 + inverseIndex]
-								]}
-							/>
-						);
-					})}
-				<pre>
-					{'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-						.split('')
-						.map((l, i) => {
-							if (offsets[0] === i) {
-								return `[${l}]`;
-							}
-							if (offsets[offsets.length - 1] === i) {
-								return `{${l}}`;
-							}
-							return ` ${l} `;
-						})
-						.reverse()
-						.join('\n')}
-				</pre>
+					.join('\n')}
+			</pre>
+			<div className="keyboards">
+				<Keyboard character={offsets[offsets.length - 1]} isLamp={true} />
+				<Keyboard
+					character={offsets[0]}
+					isLamp={false}
+					onLetterPress={letter => {
+						instance.encodeLetter(letter.toLowerCase());
+					}}
+				/>
 			</div>
-			<pre>{`Input:  [${input.length > 34 ? '…' : ''}${input
-				.toUpperCase()
-				.substring(input.length - 34, input.length)}]`}</pre>
-			<pre>{`Output: {${output.length > 34 ? '…' : ''}${output
-				.toUpperCase()
-				.substring(output.length - 34, output.length)}}`}</pre>
 		</div>
 	);
 };
